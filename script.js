66675a4df6e0183fe0270ab5ab4c13072d2092d4
pavanchambhare <pavanchambhare88@gmail.com>
2022-05-28 20:54:05 +0530

    first commit

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ function triple(x){
 
 const output = arr.map(triple);
 console.log(output)
+
+//tarnary oprator
+
+let num = 8;
+let result
+
+
+console.log( 0 == "0" ,"hhh")
+
+
+
+//  const result1 = num%2 == "0" ? "even" : "odd"//odd
+// const result2 = num%2 === 0 ? "even" : "odd"//even
+// const result3 = num%2 == 0 ? "even" : "odd"//even
+
+
+// console.log(result,result1,result2,result3)
+
+let mm  = 1 ;
+
+// Pavn is superhero
+
+for (mm = 1; mm <= 9;   mm++){
+    console.log(mm , "Code With Pavan")
+}
+console.log(mm , "Code With Pavan" )
